03330b9627d4446d3665d523bd002eab1e8ade82
Lukasz  Czajkowski <lukaszczajkowski.wroc@gmail.com>
2020-10-17 23:50:14 +0200

    Spring profiles tutorial completed

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
+import guru.springframework.sfgdi.controllers.I18nController;
 import guru.springframework.sfgdi.controllers.MyController;
 import guru.springframework.sfgdi.controllers.PropertyInjectedController;
 import guru.springframework.sfgdi.controllers.SetterInjecteController;
@@ -14,6 +15,10 @@ public class SfgDiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+		
+		I18nController i18nController =(I18nController) ctx.getBean("i18nController");
+		
+		System.out.println(i18nController.sayHello());
 		
 		MyController myController = (MyController) ctx.getBean("myController");
 		
